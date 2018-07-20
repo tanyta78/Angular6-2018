c5dfe9579be60f3aa6531846c5133f2d49f2eaa3
Tatyana Milanova <tanyta78@gmail.com>
2018-07-20 17:54:19 +0300

    components ngif ngstyle ngfor

--- udemy/basics-assignment-1-start/basics-assignment-1-start/src/app/article/article.component.ts
+++ udemy/basics-assignment-1-start/basics-assignment-1-start/src/app/article/article.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
-  title = 'My article';
+  title:string = 'My article';
+  description:string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, expedita iure. Voluptatum, mollitia debitis? Odit deleniti laborum numquam ab ipsa ipsam iusto sit a distinctio voluptates, voluptas et excepturi qui";
+  isDisplayed:boolean=false;
+  textBtnDetails='Display';
+  clickedArray=[];
+  isClicked=false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleParagraph(){
+    this.isDisplayed=!this.isDisplayed;
+    this.textBtnDetails=this.textBtnDetails==="Display"?"Hide":"Display";
+    this.isClicked=true;
+    this.clickedArray.push(this.textBtnDetails);
+  }
+
+  getColor(i){
+    return i > 4 ? "blue":"white";
+  }
 }
