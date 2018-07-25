05cbe913fd2e91ddaae629ce15021e6095dc5bd8
Tatyana Milanova <tanyta78@gmail.com>
2018-07-26 00:30:09 +0300

    FINAL_PROJECT RecipeBook ShopingList add form with ViewChild

--- recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() shopItemAdded= new EventEmitter<Ingredient>();
+  @ViewChild('amountInput') amountInput:ElementRef;
+  @ViewChild('nameInput') nameInput:ElementRef;
+
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onShopItemAdded(){
+    const newIngredient= new Ingredient(this.nameInput.nativeElement.value,this.amountInput.nativeElement.value);
+    this.shopItemAdded.emit(newIngredient);
   }
 
 }
