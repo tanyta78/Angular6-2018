9c23664ca9a4c29f8292c6ef7fbc0b8ac48850d1
Tatyana Milanova <tanyta78@gmail.com>
2018-08-05 00:21:46 +0300

    pipes- custom pipes

---
+++ udemy/pipes-assignment-start/src/app/sort.pipe.ts
@@ -0,0 +1,45 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort'
+})
+export class SortPipe implements PipeTransform {
+  Array.prototype.sortBy = function(p) {
+    return this.slice(0).sort(function(a,b) {
+      return (a[p] > b[p]) ? 1 : (a[p] < b[p]) ? -1 : 0;
+    });
+  }
+
+  transform(value: any, propName: string) {
+    if (value.length === 0 ) {
+      return value;
+    }
+   
+    value.sort(this.compareValues(propName))
+    return value;
+  }
+
+compareValues(key, order='asc') {
+    return function(a, b) {
+      if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        // property doesn't exist on either object
+          return 0; 
+      }
+  
+      const varA = (typeof a[key] === 'string') ? 
+        a[key].toUpperCase() : a[key];
+      const varB = (typeof b[key] === 'string') ? 
+        b[key].toUpperCase() : b[key];
+  
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return (
+        (order == 'desc') ? (comparison * -1) : comparison
+      );
+    }
+  }
+}
